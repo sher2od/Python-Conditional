707a069a794b44ae7b9cae37042bb4fbc3d35d91
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-03 21:28:07 +0300

    task04

--- elif04.py
+++ elif04.py
@@ -1,1 +1,17 @@
+age = int(input())
 
+summa  = 100
+if age < 7:
+    summa *= 0.5
+    print(summa)
+
+if 7 < age < 17:
+    summa *= 0.8
+    print(summa)
+
+if age < 60:
+    summa *= 0.7
+    print(summa)
+
+else:
+    print("chegirma yoq afsus")
