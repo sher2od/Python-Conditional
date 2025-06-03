69318b3b99dcc465690c01367e90f8d0da2ff75d
sher2od <sherzodqarshiboyev543@gmail.com>
2025-06-03 21:12:45 +0300

    task03

---
+++ elif03.py
@@ -0,0 +1,13 @@
+number = int(input(""))
+
+if number % 2 == 0:
+    print(" 2 ga bilinadi")
+
+if number % 3 == 0:
+    print("3 gan bolindi")
+
+if number % 5 == 0:
+    print("5 ga bolinadi")
+
+else:
+    print("bolinmaydi")
